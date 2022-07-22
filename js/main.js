b9891b00789724db3638ca94c47fbde4f5c1a619
ahmed-menisy <amelmaster16@gmail.com>
2022-07-22 02:01:56 +0200

    update 2

--- js/main.js
+++ js/main.js
@@ -50,13 +50,15 @@ document.addEventListener("scroll", function () {
    if (scrollY >= 300) {
       // change nav to fixed
       nav.classList.add("fixed-top");
+      nav.classList.add("shadow");
    } else {
       nav.classList.remove("fixed-top");
+      nav.classList.remove("shadow");
    }
    // to change active
    let curentSec = "";
    sections.forEach((section) => {
-      if (scrollY >= section.offsetTop - 100 && section.hasAttribute("id")) {
+      if (scrollY >= section.offsetTop - 90 && section.hasAttribute("id")) {
          curentSec = section.getAttribute("id");
          navLinks.forEach((link) => {
             link.classList.remove("active");
@@ -70,9 +72,16 @@ document.addEventListener("scroll", function () {
 // Add Smoth Scroll By Js
 for (let i = 0; i < navLinks.length; i++) {
    navLinks[i].addEventListener("click", function (e) {
-    scrollTo({
-      top:document.querySelector(navLinks[i].dataset.href).offsetTop - 90,
-      behavior:'smooth'
-    })
+      scrollTo({
+         top: document.querySelector(navLinks[i].dataset.href).offsetTop - 90,
+         behavior: "smooth",
+      });
    });
 }
+// Go To Top
+document.getElementById("goTop").addEventListener("click", () => {
+   this.scrollTo({
+      top: 0,
+      behavior:'smooth'
+   });
+});
